309752099b28f325cbc64ba4714daa624b287e06
Ana <anavd12@gmail.com>
2019-02-14 15:40:39 -0300

    Add files via upload

---
+++ to_Integer.py
@@ -0,0 +1,29 @@
+def char_to_int(value):
+        try:
+                int(value)
+        except ValueError:
+                return -1
+        else:
+                return int(value)
+
+
+def to_integer(value):
+        num = ""
+        x = 0
+
+        if char_to_int(value) != -1:
+                return int(value)
+        elif (value[0] == "+" or value[0] == "-") and char_to_int(value[1]) != -1:
+                num = value[0]
+                for i in range(1,len(value)):
+                        if char_to_int(value[i]) != -1:
+                                num += value[i]
+                        else: break
+                return num
+        elif char_to_int(value[0]) != -1:
+                for i in value:
+                        if char_to_int(i) != -1:
+                                num += i
+                        else: break
+                return num
+        else: return 0
